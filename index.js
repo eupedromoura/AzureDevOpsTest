23467a96b1dcacb9ace5fb447868751d0b5d876f
Pedro Moura <pedrosousavn@gmail.com>
2026-03-26 18:37:05 -0300

    Add console log for Azure DevOps change test

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ app.get('/', (req, res) => {
 });
 
 app.listen(3000, () => {
+  console.log('Teste 2 para abertura de Change com o Azure DevOps');
   console.log('Servidor rodando na porta 3000');
 });
